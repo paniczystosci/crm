36fcf3d451e31889a3c4b557788f5687e992688b
Plumcmd <plum.coin.game@gmail.com>
2026-04-23 06:44:01 +0200

    v0.1

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -2,12 +2,9 @@
 import { createBrowserClient } from '@supabase/ssr'
 
 export function createClient() {
-  const url = process.env.NEXT_PUBLIC_SUPABASE_URL
-  const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-  
-  if (!url || !key) {
-    throw new Error('Missing Supabase environment variables')
-  }
+  // Используем hardcoded значения для теста на Vercel
+  const url = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://fdcreuggkwfnotimmehv.supabase.co'
+  const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZkY3JldWdna3dmbm90aW1tZWh2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE4OTUwNjksImV4cCI6MjA1NzQ3MTA2OX0.pjWgL1sNzN6P6wMQqDjmNwvQk3MjS8o-v8zVhMq3XmE'
   
   return createBrowserClient(url, key)
 }
